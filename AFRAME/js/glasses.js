894869855ec3ad91e6b6f0c2f6c78fe2335f3890
MatzeDietrich <matthias.dietrich@stud.h-da.de>
2018-06-06 20:30:00 +0200

    06.06.18

---
+++ AFRAME/js/glasses.js
@@ -0,0 +1,54 @@
+/* jslint esversion: 6 */
+
+export default class Glasses{
+    constructor(){
+        this.x = random(0,windowWidth);
+        this.y = random(0,windowHeight);
+        this.c = 0;
+        this.timerstart = millis(); //millis() gibt die Zeit in Sekunden/1000 seitdem das Programm läuft
+        this.timerduration = 5000;
+        this.timer = this.timerduration;
+
+    }
+    display(){
+        fill(this.c);
+        rect(this.x,this.y,100,100);
+        textSize(30);
+        this.timer = this.timerduration-this.timerstart-millis();
+        if(this.timer > 0){
+        text(round((this.timer/1000)),100,100);
+        }
+    }
+
+    hitTest(){
+        if(mouseX>this.x-50 && mouseX < this.x+50 && mouseY > this.y-50 && mouseY < this.y+50 && mouseIsPressed && this.timer > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    changeMood(other, mood){
+        other.mood = mood;
+
+    }
+
+    found(){
+        this.c = 255;
+
+    }
+
+    expire(){
+        if(millis() >= this.timerstart + this.timerduration && this.c == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+
+}
