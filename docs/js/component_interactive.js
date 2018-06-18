3561695dafbe21236a98299cdfc52430e1926302
FranziBaum <franziska.baum@stud.h-da.de>
2018-06-18 13:09:33 +0200

    Zeile gelöscht

--- docs/js/component_interactive.js
+++ docs/js/component_interactive.js
@@ -4,9 +4,8 @@
 
 AFRAME.registerComponent('interactive', { //Hier wird ein Component mit dem Namen "interact" registriert.
     schema: {   //Das Schema beinhaltet die Parameter einer Komponente. In diesem Fall nur der Zustand.
-        isActive: { type: 'boolean', default: false }, //Der Parameter "isActive" beinhaltet einen boolean und ist standarweise "false".
+        isActive: { type: 'boolean', default: true }, //Der Parameter "isActive" beinhaltet einen boolean und ist standarweise "false".
         activeTime: { type: 'int', default: 10 },
-        affectMoodAmount: { type: 'int', default: 0 },
         event: {type: 'string', default: ''},
         message: {type: 'string', default: ''},
         influence: {type: 'int', default: 20}
