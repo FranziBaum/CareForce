6492b53184a852e6777c627a519962ab28e419b9
FranziBaum <franziska.baum@stud.h-da.de>
2018-06-21 15:42:16 +0200

    test

--- docs/js/component_game.js
+++ docs/js/component_game.js
@@ -8,19 +8,11 @@ AFRAME.registerComponent('game', { //Hier wird ein Component mit dem Namen "inte
         currentTime: { type: 'int', default: 0 },
         firstChallengeTime: { type: 'int', default: 10000 },
         caretime: { type: 'int', default: 20 },
-<<<<<<< HEAD
+
+        
+    
         state: { type: 'string', default: 'play' },
-=======
-<<<<<<< HEAD
-        state: { type: 'string', default: 'start' },
-=======
-<<<<<<< HEAD
-        state: { type: 'string', default: 'intro' },
-=======
-        state: { type: 'string', default: 'outro' },
->>>>>>> edf74f1a12b64fb34027e3fae5804453c52f21eb
->>>>>>> ee77f2d62403bb4b6ee23221b04634d621384280
->>>>>>> 7ef3a8f1afa042c5d261b00d3dc29f8d3d5758fb
+
         challenges: { type: 'array' }
     },
     init: function () { //Die "init"-Funktion wird zu Beginn genau 1 mal aufgerufen.
