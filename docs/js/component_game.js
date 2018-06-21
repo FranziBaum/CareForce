74720447e69d3a6d8f9efa96485de78563a38890
MatzeDietrich <matthias.dietrich@stud.h-da.de>
2018-06-21 10:23:03 +0200

    21.06.18 #1

--- docs/js/component_game.js
+++ docs/js/component_game.js
@@ -6,34 +6,59 @@ AFRAME.registerComponent('game', { //Hier wird ein Component mit dem Namen "inte
     schema: {   //Das Schema beinhaltet die Parameter einer Komponente. In diesem Fall nur der Zustand.
         startTime: { type: 'int', default: 0 },
         currentTime: { type: 'int', default: 0 },
-        firstChallengeTime: {type: 'int', default: 10000},
-        work: {type: 'int', default: 60},
-        care: {type: 'int', default: 20},
-        free: {type: 'int', default: 20},
-        challenges: {type: 'array'}
+        firstChallengeTime: { type: 'int', default: 10000 },
+        caretime: { type: 'int', default: 20 },
+        state: { type: 'string', default: 'intro' },
+        challenges: { type: 'array' }
     },
     init: function () { //Die "init"-Funktion wird zu Beginn genau 1 mal aufgerufen.
-    this.data.startTime = Date.now();
-    var challenges = document.querySelectorAll('a-collada-model[interactive]');
-    this.data.challenges = Array.from(challenges);
+        this.data.startTime = Date.now();
+        var challenges = document.querySelectorAll('a-collada-model[interactive]');
+        this.data.challenges = Array.from(challenges);
     },
     update: function () {
-    },
-    tick: function(){
-        this.data.currentTime = Date.now()-this.data.startTime;
-        console.log(this.data.currentTime);
-        if(this.data.currentTime >= this.data.firstChallengeTime){
-            this.data.startTime = Date.now();
-        var randomNumber = Math.floor(Math.random() * this.data.challenges.length);
-        var randomChallenge = this.data.challenges[randomNumber];
-        randomChallenge.setAttribute('interactive', 'isActive', true);
-        this.data.challenges.splice(randomNumber,1);
-        console.log(randomChallenge+' ist aktiv');
-        document.querySelector('#handytext').setAttribute('value', randomChallenge.id);
+        if (this.data.state == 'start') {
 
+        }
+        else if (this.data.state == 'intro') {
+
+        }
+        else if (this.data.state == 'play') {
+            var granny = document.getElementById('granny');
+            var handy = document.getElementById('handy');
+            var roomlamp = document.getElementById('roomlamp');
+
+
+            granny.setAttribute('visible', true);
+            handy.setAttribute('visible', true);
+            roomlamp.setAttribute('intensity', 0.7);
+
+
+
+
+        }
+        else if (this.data.state == 'decide') {
+
+        }
+        else if (this.data.state == 'outro') {
+
+        }
+    },
+    tick: function () {
+        if(this.data.state == 'play'){
+        this.data.currentTime = Date.now() - this.data.startTime;
+        if (this.data.currentTime >= this.data.firstChallengeTime && this.data.challenges.length > 0) {
+            this.data.startTime = Date.now();
+            var randomNumber = Math.floor(Math.random() * this.data.challenges.length);
+            var randomChallenge = this.data.challenges[randomNumber];
+            randomChallenge.setAttribute('interactive', 'isActive', true);
+            this.data.challenges.splice(randomNumber, 1);
+            console.log(randomChallenge.id + ' ist aktiv');
+            document.querySelector('#handytext').setAttribute('value', randomChallenge.id);
         }
 
     }
+}
 
 
 });
--- docs/js/component_interactive.js
+++ docs/js/component_interactive.js
@@ -5,11 +5,10 @@
 AFRAME.registerComponent('interactive', { //Hier wird ein Component mit dem Namen "interact" registriert.
     schema: {   //Das Schema beinhaltet die Parameter einer Komponente. In diesem Fall nur der Zustand.
         isActive: { type: 'boolean', default: false }, //Der Parameter "isActive" beinhaltet einen boolean und ist standarweise "false".
-        activeTime: { type: 'int', default: 10000 },
-        event: { type: 'string', default: '' },
+        activeTime: { type: 'int', default: 8000 },
         message: { type: 'string', default: '' },
         influence: { type: 'int', default: 20 },
-        startTime: {type: 'int', default: 0},
+        startTime: {type: 'int'},
         foundTime: {type: 'int'}
     },
     init: function () { //Die "init"-Funktion wird zu Beginn genau 1 mal aufgerufen.
@@ -18,21 +17,20 @@ AFRAME.registerComponent('interactive', { //Hier wird ein Component mit dem Name
         var scene = document.querySelector('a-scene');
 
         el.addEventListener('click', function () {
+            console.log("jetzt ist active: "+ data.isActive);
             if (data.isActive) {
-                el.emit(data.event); //element ruft event auf 
-                console.log(data.event);
-                data.isActive = false;
-                el.classList.remove("clickable");
+                el.emit('animate');
                 var heart = document.querySelector('#heart');
                 var heartvalue = heart.getAttribute('feedback').value;
                 heart.setAttribute('feedback', 'value', heartvalue + data.influence);
+                data.isActive = false;
+                el.classList.remove("clickable");
             }
         });
 
     },
     update: function () {
-        var cube = document.createElement('a-cube');
-        if (this.data.isActive) {
+        if (this.data.isActive == true) {
             this.el.classList.add("clickable");
             this.data.startTime = Date.now();
         }
