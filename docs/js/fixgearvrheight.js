67ce78a0b2eba77a8cfa93ed2b6ddb63a78db375
MatzeDietrich <matthias.dietrich@stud.h-da.de>
2018-07-05 12:31:07 +0200

    test

---
+++ docs/js/fixgearvrheight.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('gearvr-height-fix', {
+    dependencies: ['position'],
+  
+    init: function () {
+      if (!AFRAME.utils.isGearVR()) { return; }
+      var position = this.el.getComputedAttribute('position');
+      position.y += 1.6;
+      this.el.setAttribute('position', position);
+    }
+  });
